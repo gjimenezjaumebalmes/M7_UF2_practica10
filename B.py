64524ccd05fc605f67600be50d2e76b5ff129e3a
FHP187 <gjimenez@jaumebalmes.net>
2023-01-12 18:49:03 +0100

    1s Commit

---
+++ B.py
@@ -0,0 +1,8 @@
+# - funció que mostri, per ciutat, el total de població.
+# - funció que mostri, per ciutat, la densitat per KM2
+# - funció que mostri, per ciutat, la densitat per M2
+# - funció main la qual truqui a les 3 funcions i mostri, utilitzant matplotlib,
+# 1 gràfica circular per cada funció mostrant els resultats. (Cal que la gràfica
+# tingui llegenda i cal mostrar el % en cada “quesito”)
+
+
