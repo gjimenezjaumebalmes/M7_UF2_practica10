a5caa1fc8a43e0d1f85b3789d502136a21be63bd
FHP187 <gjimenez@jaumebalmes.net>
2023-01-17 20:42:16 +0100

    4n Commit Guillem

--- B/Exercici3.py
+++ B/Exercici3.py
@@ -11,7 +11,7 @@ def function3():
     data['Density  M2'] = data['Density  M2'].replace(',', '', regex=True)
     df = pd.DataFrame(data, columns=['City', 'Density  M2'])
     print(df[:10])
-    plt.pie(df["Density KM2"], labels=df["City"])
+    plt.pie(df["Density  M2"], labels=df["City"])
     plt.title("Ejercicio 3")
     plt.show()
 if __name__ == "__main__":
